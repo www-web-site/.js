d3604aa2b6e6734fec05d36ddd64b16148b13dc8
www-web-site <329blogertop228@gmail.com>
2025-04-18 10:59:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const askName = prompt('Ismingizni kiriting!');
+
+const askAge = prompt('Yoshingizni kiriting!');
+
+const askRegion = prompt('Manzilingizni kiriting!');
+
+let notify ='Salom mening ismim ' + askName + ' va men ' + askAge + ' yosh daman. Men ' + askRegion + ' danman ';
+
+alert (notify);
